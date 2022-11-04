f27a2c3b06aa7c695d0dff01be9061d6f44174f2
Daniel Duea <danielduea@gmail.com>
2022-11-04 17:48:21 -0500

    Rest of the components
    Created the BusinessList and SearchBar components

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './BusinessList.css';
+import '../Business/Business'
+
+class BusinessList extends React.Component {
+  render() {
+    <div className="BusinessList">
+      <Business />
+    </div>
+  }
+}
+
+export default BusinessList
